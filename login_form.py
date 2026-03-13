24f1396cf8cffab6b47c2075f5b411388455e520
Gopi <147640100+Gopikrishnan200@users.noreply.github.com>
2026-03-13 16:11:54 +0530

    Update comment for password tools section

--- login_form.py
+++ login_form.py
@@ -12,7 +12,7 @@ ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
-# ------------------ PASSWORD TOOLS ------------------
+# ------------------ PASSWORD TOOLS ------------------------------------
 def hash_password(password: str):
     return pwd_context.hash(password)
 
